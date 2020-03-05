9902c55f7c83e55a48321f0bf6bfe066516379e5
Rody_Fernandez <rody.fernandez.saa@gmail.com>
2020-03-05 08:47:45 -0300

    Ajustes de algunas lineas de codigo para mejor entendimiento

--- Semaforo.py
+++ Semaforo.py
@@ -1,23 +1,33 @@
+#importamos los módulos necesarios
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)  ## GPIO 4 ROJO
+GPIO.setup(17, GPIO.OUT) ## GPIO 17 AMARILLO
+GPIO.setup(27, GPIO.OUT) ## GPIO 27 VERDE
+rojo = 4
+amarillo = 17
+verde = 27
+tiempoCambio = 5
 
 def semaforo():
         print "Iniciando semaforo..."
-        GPIO.output(4, True)
-        time.sleep(5)
+        GPIO.output(rojo, True)
+        time.sleep(tiempoCambio)
 
-        GPIO.output(27, True)
-        GPIO.output(4, False)
-        time.sleep(5)
+        GPIO.output(verde, True)
+        GPIO.output(rojo, False)
+        time.sleep(tiempoCambio)
 
-        GPIO.output(17,True)
+        GPIO.output(amarillo,True)
         time.sleep(2)
 
-        GPIO.output(17, False)
-        GPIO.output(27, False)
+        GPIO.output(amarillo, False)
+        GPIO.output(verde, False)
 
-GPIO.cleanip()
+# Código principal desde el que usamos todas las funciones
+for b in range(3):
+    semaforo()
+
+print("Limpiando la configuración de los GPIO")
+GPIO.cleanip() ## Hago una limpieza de los GPIO
